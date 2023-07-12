b3a4d0aef8fa8c5aaa597f880991b85ce67861f8
Ali Fotouhi <mr.ali.fotouhi@gmail.com>
2023-07-12 18:36:20 +0330

    Some sec stuff

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import store from "@/store";
 
 const routes = [
   {
@@ -15,23 +16,36 @@ const routes = [
   {
     path: '/profile',
     name: 'profile',
-    component: () => import('../views/ProfileView.vue')
+    component: () => import('../views/ProfileView.vue'),
+    meta: {loginRequired: true}
   },
   {
     path: '/login',
     name: 'login',
-    component: () => import('../views/LoginView.vue')
+    component: () => import('../views/LoginView.vue'),
+    meta: {loginRedirect: true}
   },
   {
     path: '/logout',
     name: 'logout',
-    component: () => import('../views/LogoutView.vue')
+    component: () => import('../views/LogoutView.vue'),
+    meta: {loginRequired: true}
   }
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.loginRequired) && !store.state.isAuthenticated) {
+    next("/login")
+  } else if (to.matched.some(record => record.meta.loginRedirect) && store.state.isAuthenticated) {
+    next("/profile")
+  } else {
+    next()
+  }
 })
 
 export default router
