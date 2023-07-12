cd6a39f27dd150c2353f06a86c8030142e19c47c
Ali Fotouhi <mr.ali.fotouhi@gmail.com>
2023-07-13 01:16:33 +0330

    Add edit form to article page

--- src/views/ArticleView.vue
+++ src/views/ArticleView.vue
@@ -4,18 +4,48 @@
       <h3 class="fw-bold">{{ article.title }}</h3>
       <div>{{ article.content }}</div>
     </article>
+    <div v-if="$store.state.isAuthenticated">
+      <hr/>
+      <button
+          class="btn btn-outline-secondary px-4"
+          @click="editing = !editing">Edit
+      </button>
+      <button class="btn btn-outline-danger px-4 mx-3">Delete</button>
+      <Transition name="fade">
+        <div v-if="editing">
+          <hr class="my-3"/>
+          <ArticleForm form-title="Edit Article" :article="article"/>
+        </div>
+      </Transition>
+    </div>
   </div>
 </template>
 
 <script>
+import ArticleForm from "@/components/ArticleForm.vue";
+
 export default {
   name: 'Article',
+  components: {ArticleForm},
   data() {
     let articles = JSON.parse(localStorage.getItem("articles") ?? "[]")
     let article = articles.find(x => x.slug === this.$route.params.slug)
     return {
-      article: article
+      article: article,
+      editing: false
     }
   }
 }
 </script>
+
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
