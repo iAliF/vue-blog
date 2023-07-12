bef11d498558f36dabd723459eb2031f8025486b
Ali Fotouhi <mr.ali.fotouhi@gmail.com>
2023-07-13 00:43:36 +0330

    Add 'ArticleForm' component

---
+++ src/components/ArticleForm.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="ArticleForm">
+    <div class="row">
+      <div class="col-md-8 col-sm-10 mx-auto border shadow rounded-4 p-4 mt-3">
+        <h1 class="mt-3 mt-4 fw-bold text-center">{{ formTitle }}</h1>
+        <hr/>
+        <form @submit.prevent="doSend">
+          <div class="form-group mb-3">
+            <input
+                type="text"
+                class="form-control p-2"
+                placeholder="Title"
+                aria-label="Title"
+                v-model="title">
+          </div>
+          <div class="form-group mb-3">
+            <input
+                type="text"
+                class="form-control p-2"
+                placeholder="Description"
+                aria-label="Description"
+                v-model="description">
+          </div>
+          <div class="form-group mb-3">
+            <textarea
+                class="form-control"
+                v-model="content"
+                rows="9"/>
+          </div>
+          <hr/>
+          <button class="btn btn-primary" type="submit">Submit</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ArticleForm',
+  data() {
+    return {
+      title: '',
+      description: '',
+      content: '',
+    }
+  },
+  props: {
+    formTitle: String
+  },
+  emits: ["submit"],
+  methods: {
+    doSend() {
+      this.$emit('submit', this.title, this.description, this.content)
+    }
+  }
+}
+</script>
--- src/views/AddArticleView.vue
+++ src/views/AddArticleView.vue
@@ -1,59 +1,30 @@
 <template>
   <div class="AddArticle">
-    <div class="row">
-      <div class="col-md-8 col-sm-10 mx-auto border shadow rounded-4 p-4 mt-3">
-        <h1 class="mt-3 mt-4 fw-bold text-center">Add New Article</h1>
-        <hr/>
-        <form @submit.prevent="doAdd">
-          <div class="form-group mb-3">
-            <input
-                type="text"
-                class="form-control p-2"
-                placeholder="Title"
-                aria-label="Title"
-                v-model="title">
-          </div>
-          <div class="form-group mb-3">
-            <input
-                type="text"
-                class="form-control p-2"
-                placeholder="Description"
-                aria-label="Description"
-                v-model="description">
-          </div>
-          <div class="form-group mb-3">
-            <textarea
-                class="form-control"
-                v-model="content"
-                rows="9"/>
-          </div>
-          <hr/>
-          <button class="btn btn-primary" type="submit">Submit</button>
-        </form>
-      </div>
-    </div>
+    <ArticleForm @submit="doAdd" form-title="Add New Article"/>
   </div>
 </template>
 
 <script>
+import ArticleForm from "@/components/ArticleForm.vue";
+
 export default {
   name: 'AddArticle',
+  components: {
+    ArticleForm
+  },
   data() {
     let articles = JSON.parse(localStorage.getItem("articles") ?? "[]")
     return {
-      title: '',
-      description: '',
-      content: '',
       articles: articles
     }
   },
   methods: {
-    doAdd() {
+    doAdd(title, description, content) {
       let object = {
-        title: this.title,
-        slug: this.title.replaceAll(" ", "-").toLowerCase(),
-        description: this.description,
-        content: this.content
+        title: title,
+        slug: title.replaceAll(" ", "-").toLowerCase(),
+        description: description,
+        content: content
       }
       this.articles.push(object)
 
