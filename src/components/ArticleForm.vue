c87ed75b4675b53da542f9181094a3e2ad253b69
Ali Fotouhi <mr.ali.fotouhi@gmail.com>
2023-07-13 01:10:25 +0330

    Update 'ArticleForm' component

--- src/components/ArticleForm.vue
+++ src/components/ArticleForm.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="ArticleForm">
     <div class="row">
-      <div class="col-md-8 col-sm-10 border shadow rounded-4 p-4 mt-3" :class="{'mx-auto': centered}">
-        <h1 class="mt-3 mt-4 fw-bold text-center">{{ formTitle }}</h1>
+      <div
+          class="col-md-8 col-sm-10 border shadow rounded-4 p-4 mt-3 mb-3"
+          :class="{'mx-auto': centered, 'mx-3': !centered}">
+        <h1 class="mt-4 fw-bold text-center">{{ formTitle }}</h1>
         <hr/>
         <form @submit.prevent="doSend">
           <div class="form-group mb-3">
