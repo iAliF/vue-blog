60eeb81e008ce24ca7855f32ad93c6cdfcc9800b
Ali Fotouhi <mr.ali.fotouhi@gmail.com>
2023-07-12 18:46:40 +0330

    Add Login form

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,7 +1,23 @@
 <template>
   <div class="Login">
-    <h1>Login Page</h1>
-    <button class="btn btn-success" @click="doLogin">Login</button>
+    <div class="row justify-content-center">
+      <div class="col-md-6 col-sm-10">
+        <h1>Login Page</h1>
+        <form>
+          <div class="input-group mb-3">
+            <span class="input-group-text" id="user-addon">@</span>
+            <input type="text" class="form-control" placeholder="Username" aria-label="Username"
+                   aria-describedby="user-addon">
+          </div>
+          <div class="input-group mb-3">
+            <span class="input-group-text" id="passwd-addon">*</span>
+            <input type="password" class="form-control" placeholder="Password" aria-label="Password"
+                   aria-describedby="passwd-addon">
+          </div>
+          <button class="btn btn-primary" @click="doLogin">Login</button>
+        </form>
+      </div>
+    </div>
   </div>
 </template>
 
