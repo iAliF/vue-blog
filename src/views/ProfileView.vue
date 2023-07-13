ef238074891ba8ec16d528eb7e7436e221775f8b
Ali Fotouhi <mr.ali.fotouhi@gmail.com>
2023-07-14 00:16:03 +0330

    Connect ProfileView to back-end

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,5 +1,30 @@
 <template>
   <div class="Profile">
-    <h1>This is a Profile page</h1>
+    <h1>Hello {{ username }}, Welcome!</h1>
   </div>
 </template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: 'Profile',
+  data() {
+    return {
+      username: ''
+    }
+  },
+  mounted() {
+    axios
+        .get("auth/users/me/",)
+        .then(response => {
+          this.username = response.data.username
+        })
+        .catch(error => {
+          console.log(error.response.data.detail)
+          this.$store.commit("logout")
+          this.$router.push({name: "login"})
+        })
+  }
+}
+</script>
