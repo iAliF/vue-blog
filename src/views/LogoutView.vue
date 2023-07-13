60134c24682381fb92d84a2b647b034a264b7bcf
Ali Fotouhi <mr.ali.fotouhi@gmail.com>
2023-07-13 21:32:35 +0330

    Connect LogoutView to back-end

--- src/views/LogoutView.vue
+++ src/views/LogoutView.vue
@@ -1,17 +1,34 @@
 <template>
   <div class="Logout">
     <h1>Logout Page</h1>
+    <div class="alert alert-danger" v-if="logoutError"> {{ logoutError }}</div>
     <button class="btn btn-danger" @click="doLogout">Logout</button>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "Login",
+  data() {
+    return {
+      logoutError: null
+    }
+  },
   methods: {
     doLogout() {
-      this.$store.commit("logout")
-      this.$router.push({name: "login"})
+      axios
+          .post("auth/token/logout/",)
+          .then(_ => {
+            this.$store.commit("logout")
+            this.$router.push({name: "login"})
+            this.logoutError = null
+          })
+          .catch(error => {
+            console.log(error.response.data)
+            this.logoutError = error.response.data
+          })
     }
   }
 }
