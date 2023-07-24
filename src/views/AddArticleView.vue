fc32da6ec6f645a79ef06b37e5724faabfedc8cf
Ali Fotouhi <mr.ali.fotouhi@gmail.com>
2023-07-25 03:19:46 +0330

    Connect AddArticleView to api

--- src/views/AddArticleView.vue
+++ src/views/AddArticleView.vue
@@ -1,11 +1,15 @@
 <template>
   <div class="AddArticle">
+    <div v-if="error" class="alert alert-danger">
+      Something went wrong
+    </div>
     <ArticleForm @submit="doAdd" form-title="Add New Article" centered/>
   </div>
 </template>
 
 <script>
 import ArticleForm from "@/components/ArticleForm.vue";
+import axios from "axios";
 
 export default {
   name: 'AddArticle',
@@ -13,23 +17,30 @@ export default {
     ArticleForm
   },
   data() {
-    let articles = JSON.parse(localStorage.getItem("articles") ?? "[]")
     return {
-      articles: articles
+      error: false
     }
   },
   methods: {
     doAdd(title, description, content) {
-      let object = {
+      let obj = {
         title: title,
         slug: title.replaceAll(" ", "-").toLowerCase(),
         description: description,
         content: content
       }
-      this.articles.push(object)
 
-      localStorage.setItem("articles", JSON.stringify(this.articles))
-      this.$router.push({name: "article", params: {"slug": object.slug}})
+      axios
+          .post("article/", obj)
+          .then(_ => {
+            this.error = false
+            this.$router.push({name: "article", params: {"slug": obj.slug}})
+          })
+          .catch(err => {
+            console.log(err)
+            this.error = true
+          })
+
     }
   }
 }
