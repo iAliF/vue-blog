e5d5f4906a9007429d2e55f0946780352f59cb5d
Ali Fotouhi <mr.ali.fotouhi@gmail.com>
2023-08-01 02:05:19 +0330

    Update user profile page

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,6 +1,42 @@
 <template>
   <div class="Profile">
-    <h1>Hello {{ username }}, Welcome!</h1>
+    <div class="col-md-6 col-lg-4 col-sm-10 mx-auto border shadow rounded-4 p-4 mt-3">
+      <h1 class="text-center">Profile | {{ username }}</h1>
+
+      <hr/>
+
+      <ul class="nav nav-pills nav-fill">
+        <li class="nav-item">
+          <a
+              class="nav-link"
+              :class="{'active': infoActive}"
+              href="#"
+              @click="infoActive = true"
+          >User Info</a>
+        </li>
+
+        <li class="nav-item">
+          <a
+              class="nav-link"
+              :class="{'active': !infoActive}"
+              href="#"
+              @click="infoActive = false"
+          >Password</a>
+        </li>
+      </ul>
+
+      <Transition name="fade">
+        <div v-if="infoActive">
+          Change User Info
+        </div>
+      </Transition>
+      <Transition name="fade">
+        <div v-if="!infoActive">
+          Change Password
+        </div>
+      </Transition>
+
+    </div>
   </div>
 </template>
 
@@ -11,7 +47,8 @@ export default {
   name: 'Profile',
   data() {
     return {
-      username: ''
+      username: '',
+      infoActive: true,
     }
   },
   mounted() {
